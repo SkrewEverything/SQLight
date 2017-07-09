21c058b21e0b6f85780b3e18c9635e28287149fe
SkrewEverything <SkrewEverything@users.noreply.github.com>
2017-07-09 05:43:42 +0530

    initial commit

---
+++ SQLight/SQLight/SQLight.h
@@ -0,0 +1,21 @@
+//
+//  SQLight.h
+//  
+//  SQLight
+//
+//  Created by Skrew Everything on 29/06/17.
+//  Copyright © 2017 SkrewEverything. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import "sqlite3.h"
+
+//! Project version number for SQLight.
+FOUNDATION_EXPORT double SQLightVersionNumber;
+
+//! Project version string for SQLight.
+FOUNDATION_EXPORT const unsigned char SQLightVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SQLight/PublicHeader.h>
+
+
